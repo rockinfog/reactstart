7dd05e4a05be59287acca660fc022a354c5307ea
stone <stonechen@wedochina.cn>
2018-11-11 10:16:37 +0800

    下拉列表取值

--- rapp/src/App.js
+++ rapp/src/App.js
@@ -32,50 +32,52 @@ const options = [{
 
 
 const styleCascader = {
-  width:200
+  width: 200
 };
 
 
-function onChange(value) {
-  console.log(value);
-}
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name:"csj",
+      name: "csj",
       isGoing: true,
       favorSite: "tb",
-      username:"",
-      lastGistUrl:""
+      username: "",
+      lastGistUrl: "",
+      city: "",
+      sites:[]
 
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.onCityChange = this.onCityChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     const _this = this;
-    axios.get("https://api.github.com/users/octocat/gists")
-    .then(function(res)
-    {
-       console.log(res);
-       var lastGist = res.data[0];
-       _this.setState({
-         username: lastGist.owner.login,
-         lastGistUrl: lastGist.html_url
-       });
+    this.setState({
+      "sites":[{label:"google",value:"gg"},{label:"facebook",value:"fb"},{label:"taobao",value:"tb"}]
     })
-    .catch(function(error){
-      alert('get失败')
-      console.log(error);
-  });
-    
+    axios.get("https://api.github.com/users/octocat/gists")
+      .then(function (res) {
+        console.log(res);
+        var lastGist = res.data[0];
+        _this.setState({
+          username: lastGist.owner.login,
+          lastGistUrl: lastGist.html_url
+        });
+      })
+      .catch(function (error) {
+        alert('get失败')
+        console.log(error);
+      });
+
   }
- 
+
   componentWillUnmount() {
     //this.serverRequest.abort();
   }
@@ -85,26 +87,43 @@ class App extends Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     const name = target.name;
 
-    console.log("name:"+name)
+    console.log("name:" + name)
     this.setState({
       [name]: value
     });
   }
 
 
+  onCityChange(value) {
+    console.log(value);
+
+    this.setState({
+      city: value
+    });
+  }
+
+
+
   handleSubmit(event) {
-    alert('Name:' + this.state.name +'  Is Going:' + this.state.isGoing + ' Your favorite flavor is: ' + this.state.favorSite);
+    alert('Name:' + this.state.name + '  Is Going:' + this.state.isGoing + ' Your favorite flavor is: ' + this.state.favorSite
+      + ' Your city is: ' + this.state.city);
     event.preventDefault();
   }
 
-  
+
 
   render() {
+    let sites = this.state.sites
+      .map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+      ));
     return (
-    
+
       <form onSubmit={this.handleSubmit}>
-      <h3>{this.state.lastGistUrl}</h3>
-      <label>
+        <h3>{this.state.lastGistUrl}</h3>
+        <label>
           姓名:
           <input
             name="name"
@@ -120,19 +139,17 @@ class App extends Component {
             checked={this.state.isGoing}
             onChange={this.handleInputChange} />
         </label>
-        
+
         <label>
           选择您最喜欢的网站
-          <select
-            name="favorSite"
+<select  name="favorSite"
             value={this.state.favorSite} onChange={this.handleInputChange}>
-            <option value="gg">Google</option>
-            <option value="rn">Runoob</option>
-            <option value="tb">Taobao</option>
-            <option value="fb">Facebook</option>
+            {
+              sites
+            }
           </select>
         </label>
-        <Cascader options={options} onChange={onChange}  style={styleCascader} />
+        <Cascader name="city" options={options} onChange={this.onCityChange} style={styleCascader} />
         <input type="submit" value="提交" />
         <Button type="primary">Button</Button>
       </form>
